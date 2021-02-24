e91078612c5eb837aae96d991f7114fbba84e8a6
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-24 15:42:57 -0300

    Update config_webhook.sh

--- config_webhook.sh
+++ config_webhook.sh
@@ -2,6 +2,4 @@ echo "Digite a URL que está no canal #webhook do SLACK:"
 read WEBHOOK
 
 sed -i 's|inserir_webhook|'$WEBHOOK'|' Dockerfile
-# sed -i 's|lab-produtor|lab-testes|' Dockerfile
-
-export WEBHOOK_ARG=$WEBHOOK
+# sed -i 's|lab-consumidor|lab-testes|' Dockerfile
